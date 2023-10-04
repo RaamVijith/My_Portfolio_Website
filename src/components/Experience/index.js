6a65b5baeea310113e08f702501d1413362d3176
RaamVijith <raamvijith1@gmail.com>
2023-10-04 13:05:00 +0530

    done fooder

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -88,11 +88,13 @@ const index = () => {
                             <TimelineItem>
                                 <TimelineSeparator>
                                     <TimelineDot variant="outlined" color="secondary" />
-                                    {index !== experiences.length - 1 && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                    <TimelineDot variant="outlined" color="secondary" />
                                 </TimelineSeparator>
                                 <TimelineContent sx={{ py: '12px', px: 2 }}>
                                     <ExperienceCard experience={experience}/>
                                 </TimelineContent>
+                                
                             </TimelineItem>
                         ))}
                     </Timeline>
